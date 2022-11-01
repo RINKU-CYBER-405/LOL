5fbbb3bfd9e756c29a223f73fb2fce054e4b7e06
Ayan ALve <115616439+RINKU-CYBER-405@users.noreply.github.com>
2022-11-01 22:14:55 +0600

    Update LOL.py

--- LOL.py
+++ LOL.py
@@ -1,3 +1,4 @@
+import os,time
 print(' Fucking By Tor Abbu')
-os.system('xdg-open https://www.facebook.com/tera.crush.69k')
+os.system('xdg-open https://www.facebook.com/tera.crush.69k');time.sleep(1)
 import CRACK
